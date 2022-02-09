f056df84f4b4774385f0a5a931e7d46e6852ba58
administrator <administrator@gym11.local>
2022-02-09 07:45:30 +0300

    РАСПАКОВКА

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+std::pair<int, int> divmod(int f, int s){
+    std::pair<int, int> result;
+    result.first = f / s;
+    result.second = f % s;
+    return result;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // распаковка на С++
+    int A[3] = {2,4,1};
+    auto [a, b] = divmod(45, 7);
+    std::cout << "Hello, World! " << a << " " << b << std::endl;
     return 0;
 }
